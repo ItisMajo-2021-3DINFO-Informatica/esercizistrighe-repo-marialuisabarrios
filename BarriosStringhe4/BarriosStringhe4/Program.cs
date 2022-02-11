eaa7fe79c77059b02b41b8ca08f10d84f4da1e6a
marialuisabarrios <98150919+marialuisabarrios@users.noreply.github.com>
2022-02-11 10:46:29 +0100

    Esercizio 4

---
+++ BarriosStringhe4/BarriosStringhe4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BarriosStringhe4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int corte=0, medie=0, lunghe=0;
+            string parola = "";
+            while (parola!="fine")
+            {
+                Console.WriteLine("inserisci una frase o una parola ");
+                parola = Console.ReadLine();
+                int lung = parola.Length;
+                if (lung<4)
+                {
+                    corte++;
+                }
+                else if (lung<=7)
+                {
+                    medie++;
+                }
+                else
+                {
+                    lung++;
+                }
+            }
+            Console.WriteLine($" le parole minori di 4 lettere sono {corte}");
+            Console.WriteLine($"le parole con un numero di lettere minore o uguale a 7 sono {medie + corte}");
+            Console.WriteLine($"le parole composte da più di 7 lettere sono {lunghe}");
+
+        }
+    }
+}
