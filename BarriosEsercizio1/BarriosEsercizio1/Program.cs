1f4ae0a5804d41dd5e50f92f3d9cf94e7ad104cb
marialuisabarrios <98150919+marialuisabarrios@users.noreply.github.com>
2022-02-18 10:20:31 +0100

    3o esercizio

---
+++ BarriosEsercizio1/BarriosEsercizio1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BarriosEsercizio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ esercizio3/esercizio3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace esercizio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Scrivi un programma che, inserita una sequenza di parole, calcoli quante, fra le parole
+            //   inserite, iniziano e terminano con la stessa lettera;
+            int contaparola = 0;
+            string parola = "";
+            while (parola!="fine")
+            {
+                Console.WriteLine("inserisci una parola");
+                parola = Console.ReadLine();
+                
+                char primalettera = parola[0];
+                int lungh=parola.Length;
+                char ultimalettera =parola[lungh-1];
+                if (primalettera == ultimalettera) 
+                {
+                    contaparola++;
+                }
+            }
+            Console.WriteLine($"il n delle parole che iniziano e fiscano la stssa lettera è di {contaparola}");
+        }
+    }
+}
